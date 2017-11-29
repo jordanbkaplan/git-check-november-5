38fdfd4b2d7efb834639ef4a0c33d7c3ebc0c9a6
Jordan Kaplan <jordbkap@umich.edu>
2017-11-29 00:31:27 -0500

    word

---
+++ perfect_matching.py
@@ -0,0 +1,13 @@
+def perfectmatching(a,b,c,d):
+	a=int(a)
+	b=int(b)
+	c=int(c)
+	d=int(d)
+	B1vals=[a,b]
+	B2vals[c,d]
+	if a>=b and d>=c:
+		print "B1 gets S1, B2 gets S2, prices stay at zero"
+	elif b>=a and c>=d:
+		print "B1 gets S2, B2 gets S1, prices stay at zero"
+	else:
+		while 
